6f7aec4a72b3022dce9765c5fdf5c3a9f113af6e
Hieu Nguyen <xuanhieugl0901@gmail.com>
2026-03-01 18:49:45 +0700

    update bg

--- themes/duc-bds/page-templates/template-home.php
+++ themes/duc-bds/page-templates/template-home.php
@@ -2,9 +2,11 @@
 /*
 * Template Name: Home
 */
-get_header(); ?>
+get_header(); 
+$bg = get_field('background');
+?>
 
-<section style="background-image: url('https://trannghia.net/wp-content/uploads/2021/07/7-quy-tac-vang-dau-tu-dat-nen.jpg');" class="bg-cover bg-center py-10">
+<section style="background-image: url('<?php echo $bg; ?>');" class="bg-cover bg-center py-10">
 	<div class="container mx-auto">
 		<?php get_template_part('template-parts/form-search-bds'); ?>
 	</div>
